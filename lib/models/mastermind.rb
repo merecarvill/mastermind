2216e6cb145315bd3f9533a76f3f287c3d1c3390
scarvill91 <scarvill91@gmail.com>
2015-04-01 00:28:13 -0500

    implemented #compare_guess_to_secret_code, starting on #new_game

--- lib/models/mastermind.rb
+++ lib/models/mastermind.rb
@@ -1,11 +1,15 @@
 class Mastermind
-  attr_reader :guessable_colors
+  attr_accessor :secret_code
+  attr_reader :guessable_colors, :max_turns, :current_turn
 
   def initialize
     @guessable_colors = [:red, :green, :orange, :yellow, :blue, :purple]
   end
 
   def new_game
+    @secret_code = generate_code(4)
+    @max_turns = 10
+    @current_turn = 1
   end
 
   def guessable_color?(color)
@@ -13,10 +17,37 @@ class Mastermind
   end
 
   def generate_code(code_length)
-    code = []
-    code_length.times do
+    code_length.times.with_object(Array.new) do |i, code|
       code << @guessable_colors.sample
     end
-    return code
+  end
+
+  def compare_guess_to_secret_code(guess)
+    feedback = {match: 0, close: 0, miss: 0}
+    color_frequencies = @secret_code.frequencies
+    close_colors_or_misses = []
+
+    guess.length.times do |index|
+      guess_color = guess[index]
+      code_color = @secret_code[index]
+
+      if guess_color == code_color
+        feedback[:match] += 1
+        color_frequencies[code_color] -= 1
+      else
+        close_colors_or_misses << guess_color
+      end
+    end
+
+    close_colors_or_misses.each do |color|
+      if color_frequencies[color] > 0
+        feedback[:close] += 1
+        color_frequencies[color] -= 1
+      end
+    end
+
+    feedback[:miss] = guess.length - feedback[:match] - feedback[:close]
+
+    return feedback
   end
 end
--- spec/models/mastermind_spec.rb
+++ spec/models/mastermind_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Mastermind do
   let!(:mastermind) { build(:mastermind) }
-  let(:code_length) { 4 }
   let(:guessable_colors) { [:red, :green, :orange, :yellow, :blue, :purple] }
+  let(:secret_code_length) { 4 }
+  let(:max_turns) { 10 }
 
   describe '#initialize' do
 
@@ -12,10 +13,20 @@ describe Mastermind do
     end
   end
 
-  describe '#guessable_colors' do
+  describe '#new_game' do
 
-    it 'returns an array of symbols enumerating the guessable colors within games' do
-      expect(mastermind.guessable_colors).to eq guessable_colors
+    it 'takes no perameters' do
+      expect{mastermind.new_game}.not_to raise_error
+    end
+
+    it 'sets current turn to 1' do
+      mastermind.new_game
+      expect(mastermind.current_turn).to eq 1
+    end
+
+    it 'sets the max number of turns' do
+      mastermind.new_game
+      expect(mastermind.max_turns).to eq max_turns
     end
   end
 
@@ -35,28 +46,91 @@ describe Mastermind do
     end
   end
 
-  describe '#new_game' do
-
-    it 'takes no perameters' do
-      expect{mastermind.new_game}.not_to raise_error
-    end
-  end
-
   describe '#generate_code' do
-    let(:generated_code) { mastermind.generate_code(code_length) }
+    let(:generated_code) { mastermind.generate_code(secret_code_length) }
 
     it 'takes the desired length of the code' do
       expect{generated_code}.not_to raise_error
     end
 
     it 'returns an array of the input length' do
-      expect(generated_code.length).to eq code_length
+      expect(generated_code.length).to eq secret_code_length
     end
 
     it 'returns an array containing only colors that are guessable colors' do
       generated_code.uniq.each do |color|
         expect(mastermind.guessable_color?(color)).to eq true
       end
+    end
+  end
+
+  describe '#compare_guess_to_secret_code' do
+    let(:mastermind) { build(:mastermind, secret_code: [:blue, :blue, :red, :green]) }
+
+    it 'takes an array' do
+      expect{mastermind.compare_guess_to_secret_code(Array.new)}.not_to raise_error
+    end
+
+    it 'returns a hash showing num elements in guess matching, close to, or absent from secret code' do
+      feedback_types = [:match, :close, :miss]
+      guess = [:foo, :foo, :bar, :bar]
+
+      feedback = mastermind.compare_guess_to_secret_code(guess)
+
+      feedback_types.each do |type|
+        expect(feedback.has_key?(type)).to be true
+      end
+    end
+
+    it 'returns all matches if all elements in guess are in secret code and in correct position' do
+      guess = mastermind.secret_code
+      feedback = mastermind.compare_guess_to_secret_code(guess)
+
+      expect(feedback[:match]).to eq guess.length
+      expect(feedback[:close]).to eq 0
+      expect(feedback[:miss]).to eq 0
+    end
+
+    it 'returns all close if each element in guess is in secret code, but not the correct position' do
+      # code: [:blue, :blue, :red, :green]
+      #       [CLOSE, CLOSE, CLOSE, CLOSE]
+      guess = [:red, :green, :blue, :blue]
+      feedback = mastermind.compare_guess_to_secret_code(guess)
+
+      expect(feedback[:match]).to eq 0
+      expect(feedback[:close]).to eq guess.length
+      expect(feedback[:miss]).to eq 0
+    end
+
+    it 'returns all misses if no elements in guess are in secret code' do
+      guess = [:foo, :foo, :bar, :baz]
+      feedback = mastermind.compare_guess_to_secret_code(guess)
+
+      expect(feedback[:match]).to eq 0
+      expect(feedback[:close]).to eq 0
+      expect(feedback[:miss]).to eq guess.length
+    end
+
+    it 'counts close elements only if each corresponds to a distinct element in secret code' do
+      # code: [:blue, :blue, :red, :green]
+      #       [CLOSE,  MISS,  MISS,  MISS]
+      guess = [:green, :green, :green, :foo]
+      feedback = mastermind.compare_guess_to_secret_code(guess)
+
+      expect(feedback[:match]).to eq 0
+      expect(feedback[:close]).to eq 1
+      expect(feedback[:miss]).to eq guess.length - 1
+    end
+
+    it 'gives precedence to counting matches over close elements' do
+      # code: [:blue, :blue, :red, :green]
+      #       [MISS,  MISS,  MISS,  MATCH]
+      guess = [:foo, :foo, :green, :green]
+      feedback = mastermind.compare_guess_to_secret_code(guess)
+
+      expect(feedback[:match]).to eq 1
+      expect(feedback[:close]).to eq 0
+      expect(feedback[:miss]).to eq guess.length - 1
     end
   end
 end
