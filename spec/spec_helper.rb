69daa923fe03c43fb023d790d8954d8b7896d55d
scarvill91 <scarvill91@gmail.com>
2015-04-03 23:03:51 -0500

    removed references to factory girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,7 @@
 require 'rubygems'
-require 'factory_girl'
 
 require_relative '../lib/models/mastermind'
 require_relative '../lib/models/game_interface'
 require_relative '../lib/models/guess_checker'
 require_relative '../lib/models/mastermind_ai'
 require_relative '../lib/models/core_extensions'
-
-require_relative '../spec/factories/masterminds'
-
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-end
