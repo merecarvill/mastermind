a950c16f546d679933b694a6df67bc694e6f429c
scarvill91 <scarvill91@gmail.com>
2015-04-04 16:39:58 -0500

    edited game messages and formatting

--- lib/game_interface.rb
+++ lib/game_interface.rb
@@ -5,24 +5,26 @@ class GameInterface
   end
 
   def display_instructions
-    puts "The name of the game is Mastermind. \
-You must think of a #{@code_length}-element-long code derived from the following elements: \
-#{@guess_elements} \
-\
-When asked, you will enter that code, and then the computer will make several attempts to guess it. \
-You will be asked to provide feedback on each guess, identifying the number of matches, close \
-elements, and misses. \
-A 'match' is an element in a guess that exists in your code and is in the correct position. \
-A 'close' element exists in your code, but in a different position than where it occurs in the guess. \
-A 'miss' is an element in a guess that does not occur in your code. \
-Note that for there to be multiple 'close' elements of the same kind, there must be an equivalent 
-number of that type of element in the code, otherwise each extra should be counted as a 'miss'. \
-eg: A code of 'blue foo foo foo' and a guess of 'bar blue blue blue' would yeild only one 'close'. \
-Each of the remaining elements count as a 'miss'."
+    puts "The name of the game is Mastermind."
+    puts "You must think of a #{@code_length}-element-long code derived from the following \
+elements: #{@guess_elements}"
+    puts "When asked, you will enter that code, and then the computer will make several \
+attempts to guess it. You will be asked to provide feedback on each guess, identifying the \
+number of matches, close elements, and misses."
+    puts "A 'match' is an element in a guess that exists in your code and is in the correct \
+position."
+    puts "A 'close' element exists in your code, but in a different position than where it \
+occurs in the guess."
+    puts "A 'miss' is an element in a guess that does not occur in your code."
+    puts "Note that for there to be multiple 'close' elements of the same kind, there must \
+be an equivalent number of that type of element in the code, otherwise each extra should be \
+counted as a 'miss'."
+    puts "eg: A code of 'blue foo foo foo' and a guess of 'bar blue blue blue' would yeild \
+only one 'close'. Each of the remaining elements count as a 'miss'."
   end
 
   def solicit_code
-    puts "You may now enter your code, like so: '1st_element 2nd_element ... nth_element'"
+    puts "Please enter your code, like so: '1st_element 2nd_element ... nth_element'"
 
     input = STDIN.gets.strip
     input.split(" ").map{ |c| c.to_sym }
@@ -33,8 +35,8 @@ Each of the remaining elements count as a 'miss'."
   end
 
   def solicit_feedback
-    puts "Please input feedback on the most recent guess.\
-eg: If a guess had 1 match, 3 close, 0 misses, you should type '1 3 0'"
+    puts "Please input feedback on the most recent guess."
+    puts "For example, if a guess had 1 match, 3 close, 0 misses, you should type '1 3 0'"
 
     input = STDIN.gets.strip
     labels = [:match, :close, :miss]
@@ -42,7 +44,7 @@ eg: If a guess had 1 match, 3 close, 0 misses, you should type '1 3 0'"
   end
 
   def display_code_maker_won
-    puts "Your code wasn't guessed, so you win! You didn't cheat did you?" 
+    puts "Your code wasn't guessed, so you win!" 
   end
 
   def display_code_maker_lost
