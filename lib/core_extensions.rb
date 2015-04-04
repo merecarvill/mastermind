8961819f0cba242fe2454ea450d6cd2d1583c5c3
scarvill91 <scarvill91@gmail.com>
2015-04-04 18:32:27 -0500

    added useful methods to Array centered around deleting just one occurrence of a given item in the array

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -3,4 +3,14 @@ class Array
   def frequencies
     self.each_with_object(Hash.new(0)){ |key,hash| hash[key] += 1 }
   end
+
+  def delete_first(item)
+    self.delete_at(self.index(item) || self.length)
+  end
+
+  def delete_first_for_each_in(array)
+    array.each do |item|
+      self.delete_first(item)
+    end
+  end
 end
--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -1,12 +1,49 @@
 require 'spec_helper'
 
 describe Array do
+  let(:example_array) { [1, 1, 2, 3] }
 
   describe '#frequencies' do
+
     it 'returns a hash of each element to the number of times it occurs in array' do
       array = [:foo, :bar, :foo]
       output_hash = {foo: 2, bar: 1}
       expect(array.frequencies).to eq output_hash
+    end
+  end
+
+  describe '#delete_first' do
+
+    context 'if given item is in array' do
+
+      it 'deletes the first occurrence of given item from array' do
+        example_array.delete_first(1)
+        expect(example_array).to eq [1, 2, 3]
+      end
+
+      it 'returns the deleted item' do
+        expect(example_array.delete_first(1)).to eq 1
+      end
+    end
+
+    context 'if given item is not in array' do
+
+      it 'returns nil' do
+        expect(example_array.delete_first(0)).to be nil
+      end
+    end
+  end
+
+  describe '#delete_first_for_each_in' do
+
+    it 'for every item in given array, deletes the first occurrence from target array' do
+      example_array.delete_first_for_each_in([1, 2, 3])
+      expect(example_array).to eq [1]
+    end
+
+    it 'does nothing for items in given array that are not present in target array' do
+      example_array.delete_first_for_each_in([:not, :elements, :in, :the, :array])
+      expect(example_array).to eq example_array
     end
   end
 end
