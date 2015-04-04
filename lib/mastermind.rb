6b748182c912452d781402773082110ab2e09fcd
scarvill91 <scarvill91@gmail.com>
2015-04-04 16:41:31 -0500

    incorporated validations on player input

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -10,7 +10,6 @@ class Mastermind
     @guess_elements = params[:guess_elements] ||= [:red, :green, :orange, :yellow, :blue, :purple]
     @max_turns = params[:max_turns] ||= 10
     @code_length = params[:code_length] ||= 4
-    @guess_checker = GuessChecker.new(params[:secret_code] ||= generate_code)
     @ai = MastermindAI.new(@guess_elements, @code_length)
     @interface = GameInterface.new(@guess_elements, @code_length)
   end
@@ -19,13 +18,15 @@ class Mastermind
     @current_turn = 1
 
     @interface.display_instructions
-    @interface.solicit_code
+
+    secret_code = @interface.solicit_code until valid_code?(secret_code)
+    @guess_checker = GuessChecker.new(secret_code)
 
     while @current_turn <= @max_turns
       ai_guess = @ai.make_guess
       @interface.display_guess(ai_guess)
 
-      feedback = @interface.solicit_feedback
+      feedback = @interface.solicit_feedback until guess_checker.correct_feedback?(guess, feedback)
 
       if feedback[:match] == @code_length
         @interface.display_code_maker_lost
@@ -37,12 +38,6 @@ class Mastermind
     end
 
     @interface.display_code_maker_won
-  end
-
-  def generate_code
-    @code_length.times.with_object(Array.new) do |i, code|
-      code << @guess_elements.sample
-    end
   end
 
   def code_valid?(code)
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -20,10 +20,6 @@ describe Mastermind do
       expect(mastermind).to be_a Mastermind
     end
 
-    it 'makes a GuessChecker' do
-      expect(mastermind.guess_checker).to be_a GuessChecker
-    end
-
     it 'makes a MastermindAI' do
       expect(mastermind.ai).to be_a MastermindAI
     end
@@ -39,10 +35,6 @@ describe Mastermind do
         expect(mastermind.code_length).to eq default[:code_length]
         expect(mastermind.max_turns).to eq default[:max_turns]
       end
-
-      it 'generates a random secret code' do
-        expect(mastermind.secret_code).not_to eq Mastermind.new.secret_code
-      end
     end
 
     context 'when given parameters' do
@@ -50,7 +42,6 @@ describe Mastermind do
         guess_elements: [1, 2, 3],
         code_length: 5,
         max_turns: 8,
-        secret_code: [1, 1, 3, 2, 1],
       } }
       let(:mastermind_with_params) { Mastermind.new(params) }
       let(:mastermind_with_empty_params) { Mastermind.new(Hash.new) }
@@ -59,47 +50,22 @@ describe Mastermind do
         expect(mastermind_with_params.guess_elements).to eq params[:guess_elements]
         expect(mastermind_with_params.code_length).to eq params[:code_length]
         expect(mastermind_with_params.max_turns).to eq params[:max_turns]
-        expect(mastermind_with_params.secret_code).to eq params[:secret_code]
       end
 
-      context 'when any attribute is not specified in parameters' do
+      context 'when any given attribute is not specified in parameters' do
 
         it 'reverts to default behavior in setting that attribute' do
           expect(mastermind_with_empty_params.guess_elements).to eq default[:guess_elements]
           expect(mastermind_with_empty_params.code_length).to eq default[:code_length]
           expect(mastermind_with_empty_params.max_turns).to eq default[:max_turns]
-          expect(mastermind_with_empty_params.secret_code).not_to eq Mastermind.new.secret_code
         end
       end
-    end
-  end
-  
-  describe '#generate_code' do
-    let(:generated_code) { mastermind.generate_code }
-
-    it 'returns an array of the established code length' do
-      expect(generated_code.length).to eq mastermind.code_length
-    end
-
-    it 'returns an array containing only elements that are guess elements' do
-      generated_code.uniq.each do |element|
-        expect(mastermind.guess_elements.include?(element)).to eq true
-      end
-    end
-
-    it 'should not generate the same code every time it is called' do
-      different_codes = []
-      10.times do
-        different_codes << mastermind.generate_code
-      end
-
-      expect(different_codes.uniq.length).not_to eq 1
     end
   end
 
   describe '#code_valid?' do
 
-    it 'checks if code elements are valid guess elements' do
+    it 'checks if each of the given code\'s elements are valid' do
       invalid_elements_code = Array.new(default[:code_length], :foo)
 
       expect(mastermind.code_valid?(example_code)).to eq true
