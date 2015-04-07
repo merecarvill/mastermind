47d655fe8f857c21073e92cf1b371233286dadc9
scarvill91 <scarvill91@gmail.com>
2015-04-07 01:12:58 -0500

    made game interface easier to test precisely, improved tests

--- lib/game_interface.rb
+++ lib/game_interface.rb
@@ -1,56 +1,27 @@
 class GameInterface
-  def initialize(code_elements, code_length)
-    @code_elements = code_elements
-    @code_length = code_length
+  def initialize(code_elements, code_length, input_stream, output_stream)
+    @input_stream = input_stream
+    @output_stream = output_stream
+    @game_text = generate_game_text(code_elements, code_length)
   end
 
   def display_instructions
-    puts
-    puts "The name of the game is Mastermind."
-    puts
-    puts "===== INSTRUCTIONS ====="
-    puts "You must think of a #{@code_length}-element-long code derived from the following \
-elements: " + @code_elements.join(", ")
-    puts "Your guess can contain duplicate elements."
-    puts
-    puts "When asked, you will enter that code and the computer will make several attempts \
-to guess it. You will be asked to provide feedback on each guess, identifying the number of \
-matches, close elements, and misses."
-    puts
-    puts "A 'match' is an element in a guess that exists in your code and is in the correct \
-position."
-    puts "A 'close' element exists in your code, but in a different position than where it \
-occurs in the guess."
-    puts "A 'miss' is an element in a guess that does not occur in your code."
-    puts
-    puts "Note that for there to be multiple 'close' elements of the same kind, there must \
-be an equivalent number of that type of element in the code, otherwise each extra should be \
-counted as a 'miss'."
-    puts "eg: A code of 'blue foo foo foo' and a guess of 'bar blue blue blue' would yeild \
-only one 'close'. Each of the remaining elements count as a 'miss'."
-    puts 
+    @output_stream.puts text_for(:display_instructions)
   end
 
   def solicit_code
-    puts "Please enter your code, like so: '1st_element 2nd_element ... nth_element'"
+    @output_stream.puts text_for(:solicit_code)
 
-    input = STDIN.gets.strip
+    input = @input_stream.gets.strip
     input.split(" ").map{ |c| c.to_sym }
   end
 
-  def clear_screen
-    system("cls")
-    system("clear")
-  end
-
   def display_guess(code)
-    puts
-    puts "The computer's guess is: " + code_to_s(code)
+    @output_stream.puts ["", "The computer's guess is: " + code_to_s(code)]
   end
 
   def display_code_reminder(code)
-    puts "Remember, your code was: " + code_to_s(code)
-    puts
+    @output_stream.puts text_for(:display_code_reminder) + code_to_s(code)
   end
 
   def code_to_s(code)
@@ -58,10 +29,9 @@ only one 'close'. Each of the remaining elements count as a 'miss'."
   end
 
   def solicit_feedback
+    @output_stream.puts text_for(:solicit_feedback)
+
     feedback = Hash.new
-
-    puts "Please input feedback on the most recent guess."
-
     feedback[:match] = solicit_feedback_aspect(:match)
     feedback[:close] = solicit_feedback_aspect(:close)
     feedback[:miss] = solicit_feedback_aspect(:miss)
@@ -70,17 +40,65 @@ only one 'close'. Each of the remaining elements count as a 'miss'."
 
   def solicit_feedback_aspect(aspect)
     grammatical_insert = aspect == :close ? "" : "a "
-    print "How many elements were #{grammatical_insert + aspect.to_s}? "
-    return STDIN.gets.strip.to_i
+    @output_stream.print "How many elements were #{grammatical_insert + aspect.to_s}? "
+    return @input_stream.gets.strip.to_i
   end
 
   def display_code_maker_won
-    puts
-    puts "Your code wasn't guessed, so you win!" 
+    @output_stream.puts text_for(:display_code_maker_won)
   end
 
   def display_code_maker_lost
-    puts
-    puts "Your code was guessed, so you lose! There's always next time." 
+    @output_stream.puts text_for(:display_code_maker_lost)
+  end
+
+  def clear_screen
+    system("cls")
+    system("clear")
+  end
+
+  def text_for(method)
+    @game_text[method.to_sym]
+  end
+
+  def generate_game_text(code_elements, code_length)
+    game_text = Hash.new 
+    game_text[:display_instructions] = <<-eos
+The name of the game is Mastermind.
+
+===== INSTRUCTIONS =====
+You must think of a #{code_length}-element-long code derived from the following elements: #{code_elements.join(", ")}
+Your guess can contain duplicate elements.
+
+When asked, you will enter that code and the computer will make several attempts to guess it. You will be asked to provide feedback on each guess, identifying the number of matches, close elements, and misses.
+
+A 'match' is an element in a guess that exists in your code and is in the correct position.
+A 'close' element exists in your code, but in a different position than where it occurs in the guess.
+A 'miss' is an element in a guess that does not occur in your code.
+
+Note that for there to be multiple 'close' elements of the same kind, there must be an equivalent number of that type of element in the code, otherwise each extra should be counted as a 'miss'.
+eg: A code of 'blue foo foo foo' and a guess of 'bar blue blue blue' would yeild only one 'close'. Each of the remaining elements count as a 'miss'.
+
+eos
+    game_text[:solicit_code] = <<-eos
+Please enter your code, like so: '1st_element 2nd_element ... nth_element'
+
+eos
+    game_text[:display_code_reminder] = <<-eos
+Remember, your code was: 
+eos
+    game_text[:solicit_feedback] = <<-eos
+
+Please input feedback on the most recent guess.
+eos
+    game_text[:display_code_maker_won] = <<-eos
+
+Your code wasn't guessed, so you win!
+eos
+    game_text[:display_code_maker_lost] = <<-eos
+
+Your code was guessed, so you lose! There's always next time.
+eos
+    return game_text
   end
 end
--- spec/game_interface_spec.rb
+++ spec/game_interface_spec.rb
@@ -2,190 +2,159 @@ require 'spec_helper'
 
 describe GameInterface do
   include_context 'default_values'
-  
-  before :all do
-    $stdout = StringIO.new
-  end
-  before :each do
-    $stdout.flush
-  end
-  after :all do
-    $stdout = STDOUT
-  end
 
   let(:example_code) { [:blue, :blue, :red, :green] }
   let(:example_code_input) { "blue blue red green" }
-  let(:interface) { GameInterface.new(default[:code_elements], default[:code_length]) }
+
+  before :all do
+    @input_stream = StringIO.new
+    @output_stream = StringIO.new
+    @interface = GameInterface.new(@default_code_elements, @default_code_length, @input_stream, @output_stream)
+  end
+
+  after do
+    @input_stream.string = ""
+    @output_stream.string = ""
+  end
 
   describe '#initialize' do
 
-    it 'takes a game\'s guess elements and code length' do
-      expect{interface}.not_to raise_error
-    end
-
     it 'returns an object of type GameInterface' do
-      expect(interface).to be_a GameInterface
+      expect(@interface).to be_a GameInterface
     end
   end
 
   describe '#display_instructions' do
     before do
-      interface.display_instructions
+      @interface.display_instructions
     end
 
     it 'prints the game instructions' do
-      expect($stdout.string).not_to eq ""
-      expect($stdout.string.downcase.include?("instructions")).to be true
-    end
-
-    it 'includes the guess elements in the instructions' do
-      expect($stdout.string.include?(default[:code_elements].join(", "))).to be true
-    end
-
-    it 'includes the code length in the instructions' do
-      expect($stdout.string.include?(default[:code_length].to_s)).to be true
+      expect(@output_stream.string).to eq @interface.text_for(:display_instructions)
     end
   end
 
   describe '#solicit_code' do
+    before do
+      @input_stream.string = example_code_input
+    end
 
     it 'prints a message soliciting the player\'s secret code' do
-      allow($stdin).to receive(:gets) { example_code_input }
-      interface.solicit_code
-      expect($stdout.string).not_to eq ""
+      @interface.solicit_code
+      expect(@output_stream.string).to eq @interface.text_for(:solicit_code)
     end
 
     it 'returns the code given by the player' do
-      allow($stdin).to receive(:gets) { example_code_input }
-      expect(interface.solicit_code).to eq example_code
+      expect(@interface.solicit_code).to eq example_code
     end
   end
 
   describe '#display_guess' do
     before do
-      interface.display_guess(example_code)
+      @interface.display_guess(example_code)
     end
 
     it 'prints a given guess, including all guess elements' do
-      expect($stdout.string).not_to eq ""
+      expect(@output_stream.string).not_to eq ""
       example_code.each do |element|
-        expect($stdout.string.include?(element.to_s)).to be true
+        expect(@output_stream.string.include?(element.to_s)).to be true
       end
     end
 
     it 'at least mentions "computer" and "guess"' do
-      expect($stdout.string.include?("computer")).to be true
-      expect($stdout.string.include?("guess")).to be true
+      expect(@output_stream.string.include?("computer")).to be true
+      expect(@output_stream.string.include?("guess")).to be true
     end
   end
 
   describe '#display_code_reminder' do
     before do
-      interface.display_code_reminder(example_code)
+      @interface.display_code_reminder(example_code)
     end
 
     it 'prints a reminder of the given secret code, including all code elements' do
-      expect($stdout.string).not_to eq ""
+      expect(@output_stream.string.start_with?(@interface.text_for(:display_code_reminder))).to be true
       example_code.each do |element|
-        expect($stdout.string.include?(element.to_s)).to be true
+        expect(@output_stream.string.include?(element.to_s)).to be true
       end
-    end
-
-    it 'at least mentions "your" and "code"' do
-      expect($stdout.string.include?("your")).to be true
-      expect($stdout.string.include?("code")).to be true
     end
   end
 
   describe '#solicit_feedback' do
     let(:example_feedback) { {match: 1, close: 3, miss: 0} }
-    let(:example_feedback_input) { ["1 match", "3close", "0"] }
+    let(:example_feedback_input) { ["1 match ", "3close!", "0"] }
+    before do
+      @input_stream.string = example_feedback_input.join("\n")
+    end
 
     it 'prints a message soliciting feedback on a guess' do
-      allow($stdin).to receive(:gets) { example_feedback_input.shift }
-      interface.solicit_feedback
-      expect($stdout.string).not_to eq ""
+      @interface.solicit_feedback
+      expect(@output_stream.string.start_with?(@interface.text_for(:solicit_feedback))).to be true
     end
 
     it 'returns a feedback hash made from string input by the player' do
-      allow($stdin).to receive(:gets) { example_feedback_input.shift }
-      expect(interface.solicit_feedback).to eq example_feedback
+      expect(@interface.solicit_feedback).to eq example_feedback
     end
   end
 
   describe '#solicit_feedback_aspect' do
+    let(:aspects) { [:match, :close, :miss] }
 
     it 'prints a prompt asking player to input the given aspect of feedback' do
-      allow($stdin).to receive(:gets) { "2" }
+      @input_stream.string = "2"
 
-      interface.solicit_feedback_aspect(:foo)
-      expect($stdout.string).not_to eq ""
+      @interface.solicit_feedback_aspect(:foo)
+      expect(@output_stream.string).not_to eq ""
     end
 
     it 'specifies the aspect solicited in the message' do
-      allow($stdin).to receive(:gets) { "2" }
-
-      interface.solicit_feedback_aspect(:match)
-      expect($stdout.string.include?("match")).to be true
-      $stdout.flush
-
-      interface.solicit_feedback_aspect(:close)
-      expect($stdout.string.include?("close")).to be true
-      $stdout.flush
-
-      interface.solicit_feedback_aspect(:miss)
-      expect($stdout.string.include?("miss")).to be true
+      aspects.each do |aspect|
+        @input_stream.string = "2"
+        @interface.solicit_feedback_aspect(aspect)
+        expect(@output_stream.string.include?(aspect.to_s)).to be true
+        @output_stream.flush
+      end
     end
 
     it 'returns player input in integer form' do
-      allow($stdin).to receive(:gets) { "2" }
+      @input_stream.string = "2"
 
-      player_input = interface.solicit_feedback_aspect(:foo)
+      player_input = @interface.solicit_feedback_aspect(:foo)
       expect(player_input).to eq 2
     end
 
     it 'returns the numerical component as an integer if player input begins with a number' do
-      allow($stdin).to receive(:gets) { "2 matches" }
+      @input_stream.string = "2 matches"
 
-      player_input = interface.solicit_feedback_aspect(:foo)
+      player_input = @interface.solicit_feedback_aspect(:foo)
       expect(player_input).to eq 2
     end
 
     it 'returns the integer 0 when player input does not begin with a numerical element' do
-      allow($stdin).to receive(:gets) { "f8" }
+      @input_stream.string = "f8"
 
-      player_input = interface.solicit_feedback_aspect(:foo)
+      player_input = @interface.solicit_feedback_aspect(:foo)
       expect(player_input).to eq 0
     end
   end
 
   describe '#display_code_maker_won' do
     before do
-      interface.display_code_maker_won
+      @interface.display_code_maker_won
     end
 
     it 'prints a message telling the player they won' do
-      expect($stdout.string).not_to eq ""
-    end
-
-    it 'at least mentions "you" and "won"' do
-      expect($stdout.string.include?("you")).to be true
-      expect($stdout.string.include?("win")).to be true
+      expect(@output_stream.string).to eq @interface.text_for(:display_code_maker_won)
     end
   end
 
   describe '#display_code_maker_lost' do
     before do
-      interface.display_code_maker_lost
+      @interface.display_code_maker_lost
     end
 
     it 'prints a message telling the player they lost' do
-      expect($stdout.string).not_to eq ""
-    end
-
-    it 'at least mentions "you" and "lose"' do
-      expect($stdout.string.include?("you")).to be true
-      expect($stdout.string.include?("lose")).to be true
+      expect(@output_stream.string).to eq @interface.text_for(:display_code_maker_lost)
     end
   end
 end
